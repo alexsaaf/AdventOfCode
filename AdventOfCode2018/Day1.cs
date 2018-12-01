dae1294c27cac28b5149b7ac533960c5f5dbd621
alexsaaf <alexandersaaf95@gmail.com>
2018-12-01 09:25:39 +0100

    Add day 1

---
+++ AdventOfCode2018/Day1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2018 {
+    class Day1 {
+
+        public float TaskA(string[] input) {
+            long result = 0;
+
+            foreach (string change in input) {
+                var digit = Int32.Parse(change.Substring(1));
+                if (change[0] == '+') {
+                    result += digit;
+                } else {
+                    result -= digit;
+                }
+            }
+
+            return result;
+        }
+
+        public float TaskB(string[] input) {
+            HashSet<long> foundFreq = new HashSet<long>();
+            long result = 0;
+            foundFreq.Add(0);
+
+            while (true) {
+
+            foreach (string change in input) {
+                var digit = Int32.Parse(change.Substring(1));
+                if (change[0] == '+') {
+                    result += digit;
+                } else {
+                    result -= digit;
+                }
+                if (foundFreq.Contains(result)) {
+                    return result;
+                }
+                foundFreq.Add(result);
+            }
+
+            }
+        }
+    }
+}
---
+++ AdventOfCode2018/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace AdventOfCode2018 {
+    class Program {
+        static void Main(string[] args) {
+            Stopwatch sw = new Stopwatch();
+
+            #region Day1
+            Day1 day1 = new Day1();
+            sw.Start();
+            var day1taskA = day1.TaskA(ReadLinesFromFile("Inputs/Day1Input.txt"));
+            var day1TaskB = day1.TaskB(ReadLinesFromFile("Inputs/Day1Input.txt"));
+            sw.Stop();
+            Console.WriteLine("--- Day 1: Inverse Captcha ---");
+            Console.WriteLine("Answer to part 1 is: " + day1taskA);
+            Console.WriteLine("Answer to part 2 is: " + day1TaskB);
+            Console.WriteLine("Time elapsed: " + sw.Elapsed.Milliseconds + "\n");
+            #endregion
+
+            Console.ReadLine();
+        }
+
+        static string[] ReadLinesFromFile(string fileName) {
+            //Open the file
+            StreamReader file = new System.IO.StreamReader(fileName);
+
+            //Read all the lines
+            List<string> instructions = new List<string>();
+            string line;
+            while ((line = file.ReadLine()) != null) {
+                instructions.Add(line);
+            }
+
+            //Return them as array
+            return instructions.ToArray();
+        }
+    }
+}
