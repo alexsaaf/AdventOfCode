bc896cfc83b3db7f551e44d1917d5e391d661085
Alexander <alexandersaaf95@gmail.com>
2019-12-01 20:43:15 +0100

    add 2019

---
+++ AdventOfCode2019/Day1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfCode2019 {
+    class Day1 {
+            
+        public static double TaskA(string[] input) {
+            IEnumerable<string> modules = new List<string>(input);
+            return modules.Select(module => CalculateFuel(double.Parse(module))).Sum();
+        }
+
+
+        static double CalculateFuel(double mass) {
+            return Math.Floor(mass / 3) - 2;
+        }
+
+        public static double TaskB(string[] input) {
+            IEnumerable<string> modules = new List<string>(input);
+
+            double fuel = 0;
+
+            foreach (double module in modules.Select(module => double.Parse(module))) {
+                double requiredFule = CalculateFuel(module);
+                fuel += requiredFule;
+
+                double additionalFule = CalculateFuel(requiredFule);
+                while (additionalFule > 0) {
+                    fuel += additionalFule;
+                    additionalFule = CalculateFuel(additionalFule);
+                }
+            }
+
+            return fuel;
+        }
+
+    }
+}
---
+++ AdventOfCode2019/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace AdventOfCode2019 {
+    class Program {
+        static void Main(string[] args) {
+            Stopwatch sw = new Stopwatch();
+
+            #region Day1
+            sw.Start();
+            var day1taskA = Day1.TaskA(ReadLinesFromFile("Inputs/Day1Input.txt"));
+            var day1TaskB = Day1.TaskB(ReadLinesFromFile("Inputs/Day1Input.txt"));
+            sw.Stop();
+            Console.WriteLine("--- Day 1: Chronal Calibration ---");
+            Console.WriteLine("Answer to part 1 is: " + day1taskA.ToString());
+            Console.WriteLine("Answer to part 2 is: " + day1TaskB.ToString());
+            Console.WriteLine("Time elapsed: " + sw.Elapsed.Milliseconds + "\n");
+            #endregion
+
+
+
+
+        }
+
+
+        static string[] ReadLinesFromFile(string fileName) {
+            //Open the file
+            StreamReader file = new System.IO.StreamReader(fileName);
+
+            //Read all the lines
+            List<string> instructions = new List<string>();
+            string line;
+            while ((line = file.ReadLine()) != null) {
+                instructions.Add(line);
+            }
+
+            //Return them as array
+            return instructions.ToArray();
+        }
+    }
+}
